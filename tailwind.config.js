0c20bf7add119948447355df6f2113f2095f3cdc
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-05-21 17:31:15 +0500

    ASD

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        newcolor: {
+          DEFAULT: "#1c64f2", // Default shade
+          50: "#e3e8ff",
+          100: "#c7d1ff",
+          200: "#8f9cff",
+          300: "#5768ff",
+          400: "#2e4dff",
+          500: "#1c64f2",
+          600: "#1951c1",
+          700: "#143e90",
+          800: "#0f2b60",
+          900: "#0a1830",
+        },
+      },
+    },
   },
-  plugins: [require('daisyui')],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#F7D05E", // Reference the new color
+          // Other theme settings...
+        },
+      },
+    ],
+  },
+};
