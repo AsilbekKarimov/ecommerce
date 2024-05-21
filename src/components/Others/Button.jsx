1e604e9cefffe0029e14724a6f65bd980be7b289
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-05-21 18:22:07 +0500

    Router started

--- src/components/Others/Button.jsx
+++ src/components/Others/Button.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ text }) => {
+  return <button></button>;
+};
 
-export default Button
+export default Button;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <header></header>
       <main></main>
       <footer></footer>
-    </div>
+    </>
   );
 };
 
