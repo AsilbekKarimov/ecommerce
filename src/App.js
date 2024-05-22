f7bdb74a9f276c6648bf51a290acf757ace7c32a
MukhamedovK <kamoliddin2619@gmail.com>
2024-05-22 23:40:16 +0500

    maked swipers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+
+import MainSwiper from './components/SwiperJS/MainSwiper';
+import ClothesSwiper from './components/SwiperJS/ClothesSwiper';
+
+const bannerSlides = [
+  {
+    image: '/swiper/swiper-img.png',
+    title: 'Большая зимняя распродажа от LeoKid',
+    description: 'Конверт Leokid Classic подходит для использования в любых колясках, автокреслах и санках. Для прогулок осенью, зимой, весной и даже прохладным летом.',
+  },
+  {
+    image: '/swiper/swiper-img.png',
+    title: 'Большая зимняя распродажа от LeoKid',
+    description: 'Конверт Leokid Classic подходит для использования в любых колясках, автокреслах и санках. Для прогулок осенью, зимой, весной и даже прохладным летом.',
+  },
+];
+
+const cardSlides = [
+  {
+    image: '/swiper/cardswiper.png',
+    button: 'Подборка №106',
+    price: '1 221р',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    button: 'Подборка №1222',
+    price: '9 999р',
+  },
+];
+
+const clothesSlides = [
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+  {
+    image: '/swiper/cardswiper.png',
+    name: 'Подборка №106',
+  },
+]
+
 
 function App() {
   return (
-    <div className="App">
-        <h1 className='text-primary'>Hello</h1>
+    <div className="App h-full mx-auto w-[90%] flex flex-col items-center justify-center gap-[20px]">
+      <div className='h-full mx-auto w-[90%] flex items-center justify-center gap-[20px]'>
+        <MainSwiper width="70%" slides={bannerSlides} delay={3000} />
+        <MainSwiper width="20%" slides={cardSlides} delay={1500} />
+      </div>
+      <ClothesSwiper slides={clothesSlides} width="90%" />
     </div>
   );
 }
---
+++ src/components/SwiperJS/ClothesSwiper.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+
+const ClothesSwiper = ({ slides, width }) => {
+  return (
+    <div className="flex items-center justify-center mt-[144px]" style={{ width }}>
+      <Swiper
+        slidesPerView={5}
+        slidesPerGroup={5}
+        spaceBetween={20}
+        loop={true}
+        speed={1000}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper h-[350px]">
+        {slides.map((slide, id) => (
+          <SwiperSlide key={id}>
+            <div className='flex flex-col items-center justify-center gap-[15px]'>
+              <img src={slide.image} alt='swiper-img' className="w-full h-full" />
+              <p className='text-[20px] text-center text-[#5A5A5A] font-semibold leading-[24px] '>{slide.name}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default ClothesSwiper
