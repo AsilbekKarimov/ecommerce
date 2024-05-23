d1be47d6cc1eb13a4c98e7ced825cca664d51aad
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-05-23 17:58:50 +0500

    asd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,30 +15,32 @@ const Navbar = ({ auth }) => {
   ];
 
   return (
-    <nav className='container mx-auto flex justify-between items-center'>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-      </div>
-
-      <div className='flex flex-col items-center'>
-        <div className='relative top-8 right-[240px]'>
-          <img src={search} alt="search" />
-        </div>
-        <input className='pr-[300px] pl-14 text-[16px] py-2 rounded-3xl bg-slate-200 ' type="text" placeholder="Начните писать" />
-        <button className='bg-yellow-500 hover:bg-slate-700 hover:text-yellow-500 duration-300 px-10 py-2 rounded-e-3xl relative bottom-10 left-[225px] font-bold text-white'>Найти</button>
-      </div>
-
-      <div className='flex gap-24 items-center'>
-        {links.map((link, index) => (
-          <Link className='flex flex-col text-[10px] font-normal text-center items-center gap-2' key={index} to={link.to}>
-            <div>
-              <img src={link.imgSrc} alt={link.alt} />
-            </div>
-            {link.text}
+    <nav className='bg-white fixed z-50'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <div>
+          <Link to="/">
+            <img src={logo} alt="logo" />
           </Link>
-        ))}
+        </div>
+
+        <div className='flex flex-col items-center'>
+          <div className='relative top-8 right-[240px]'>
+            <img src={search} alt="search" />
+          </div>
+          <input className='pr-[300px] pl-14 text-[16px] py-2 rounded-3xl bg-slate-200 ' type="text" placeholder="Начните писать" />
+          <button className='bg-yellow-500 hover:bg-slate-700 hover:text-yellow-500 duration-300 px-10 py-2 rounded-e-3xl relative bottom-10 left-[225px] font-bold text-white'>Найти</button>
+        </div>
+
+        <div className='flex gap-24 items-center'>
+          {links.map((link, index) => (
+            <Link className='flex flex-col text-[10px] font-normal text-center items-center gap-2' key={index} to={link.to}>
+              <div>
+                <img src={link.imgSrc} alt={link.alt} />
+              </div>
+              {link.text}
+            </Link>
+          ))}
+        </div>
       </div>
     </nav>
   );
