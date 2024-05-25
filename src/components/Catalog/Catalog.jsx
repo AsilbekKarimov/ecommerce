107e0b41cfb707b8d0afd49bf7d6673e0e813df1
shahzoda0707 <kamilamirhanova07@gmail.com>
2024-05-25 16:18:07 +0300

    ecommerce

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -6,23 +6,23 @@ const Catalog = () => {
   return (
     <div className="sm:w-full lg:w-[90%] mx-auto py-[70px]">
       <div className="flex gap-[20px] lg:flex-row sm:flex-col">
-      <div>
-        <CatalogCard image={"/catalog-images/4.png"} categoryName={"АКСЕССУАРЫ"}/>
-      </div>
-
-      <div className="flex flex-col gap-[20px]">
-        <div>
-          <CatalogCard image={"/catalog-images/3.png"} categoryName={"КОНВЕРТЫ И ПЛЕДЫ"} />
+        <div className="flex-1">
+          <CatalogCard image={"/catalog-images/4.png"} categoryName={"АКСЕССУАРЫ"} />
         </div>
-        <div className="flex gap-[28px] lg:flex-row sm:flex-col">
-          <CatalogCard image={"/catalog-images/5.png"} categoryName={"ОДЕЖДА"} />
-          <CatalogCard image={"/catalog-images/1.png"} categoryName={"ПЕЛЕНКИ"} />
-        </div>
-      </div>
 
-      <div>
-        <CatalogCard image={"/catalog-images/2.png"} categoryName={"ГОТОВЫЕ ПОДБОРКИ"} />
-      </div>
+        <div className="flex flex-col gap-[20px] flex-1">
+          <div flex-1>
+            <CatalogCard image={"/catalog-images/3.png"} categoryName={"КОНВЕРТЫ И ПЛЕДЫ"} />
+          </div>
+          <div className="flex gap-[28px] lg:flex-row sm:flex-col flex-1">
+            <CatalogCard image={"/catalog-images/5.png"} categoryName={"ОДЕЖДА"} />
+            <CatalogCard image={"/catalog-images/1.png"} categoryName={"ПЕЛЕНКИ"} />
+          </div>
+        </div>
+
+        <div className="flex-1">
+          <CatalogCard image={"/catalog-images/2.png"} categoryName={"ГОТОВЫЕ ПОДБОРКИ"} />
+        </div>
 
       </div>
     </div>
