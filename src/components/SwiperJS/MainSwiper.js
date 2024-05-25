0b3dfab3fa6f2e38c8b8420190d70f72e8807c58
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-05-25 17:57:21 +0500

    Fixed swiper

--- src/components/SwiperJS/MainSwiper.js
+++ src/components/SwiperJS/MainSwiper.js
@@ -7,7 +7,7 @@ import 'swiper/css';
 
 const MainSwiper = ({ width, slides, delay }) => {
   return (
-    <div className={`flex items-center justify-center mt-[30px]`} style={{ width }}>
+    <div className={`flex items-center justify-center mt-[180px]`} style={{ width }}>
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -22,7 +22,7 @@ const MainSwiper = ({ width, slides, delay }) => {
       >
         {slides.map((slide, id) => (
           <SwiperSlide key={id} className="relative">
-            <img src={slide.image} alt='swiper-img' className="w-full h-full object-cover" />
+            <img src={slide.image} alt='swiper-img' className="w-full h-full object-cover rounded-[10px]" />
             {slide.title && slide.description && (
               <div className='absolute left-0 top-1/2 transform -translate-y-1/2 flex flex-col gap-[25px] text-start pl-5'>
                 <h1 className='w-[440px] font-bold text-[36px] text-[#33394F] leading-[44px]'>{slide.title}</h1>
