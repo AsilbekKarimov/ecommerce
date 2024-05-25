6141f0b68a069c395180c91a9a2942cd2f75edd3
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-05-25 18:00:34 +0500

    test

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import NavbarInfo from "./component/Navbarinfo/NavbarInfo";
 
 function App() {
   return (
     <div>
       <header>
-      <NavbarInfo />
-
         <Navbar />
       </header>
-      
       <main>
         <Outlet />
       </main>
