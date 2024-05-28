461b8fe0a4241e3ec4660d564b0c4d088bf829c0
Shaylavovsh <topovayaladaiada@gmail.com>
2024-05-28 18:11:30 +0500

    erors

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div></div>
+    <div className=' max-w-[85%] mx-auto  mt-[14%] '>
+      <div className="flex flex-col w-full text-start">
+  <div className="grid h-11 w-[1500px] card rounded-box place-items-center -left-32 bg-[#F4F4F4] relative justify-start "><p className='text-[#33394F9E] relative left-32 '>Главная - О компании</p></div> 
+  <div className="divider"></div> 
+  <div className="grid h-20 card bg-base-300 rounded-box place-items-center"></div>
+</div>
+      <p className='text-[#33394F]  '>
+      Наш магазин Lalastore.ru существует с 2019 года и всегда рад приветствовать Мам и Пап, своей теплой атмосферой. В нашем магазине, Вы всегда найдете качественную продукцию для Ваших малышей, на любое время года. У нас работают настоящие профессионалы, любящие и знающие свое дело, которые всегда помогут Вам. Покупка товара проста не только на первый взгляд, но так же легка в нашем магазине. Мы ценим каждого покупателя и знаем как осуществить Ваши покупки по самым приятным ценам.  
+      </p>
+    </div>
   )
 }
 
